f51766d840290ebbdc917e6cd98499cac811a4af
KanoMrhr <sao.tkg.17@gmail.com>
2020-05-08 12:19:54 +0900

    csv形式での結果のみ出力を追加

--- scikit-learn_k-means/ReadMe.md
+++ scikit-learn_k-means/ReadMe.md
@@ -5,3 +5,6 @@ scikit-learnとk-means法で、画像のクラス分類を行う。
 jpg_convert.pyは、keras_adamのフォルダにある画像を取り出し、
 jpg_convertフォルダに大きさをそろえて保存するプログラムである。
 保存容量が大きくなりそうなので、ここではフォルダのアップロードは割愛する。
+
+jpg_group_resultは.csv形式で
+分類結果と画像名のみを出力したもの
--- scikit-learn_k-means/test1.py
+++ scikit-learn_k-means/test1.py
@@ -7,6 +7,9 @@ import shutil  # ファイル操作用
 from skimage import io  # 手もとの画像を使うとき？はdataではできなさそうだった。
 from sklearn.cluster import KMeans
 
+import pandas as pd  # ログをcsvでも保管しておく
+# https://pythondatascience.plavox.info/pandas/%E3%83%87%E3%83%BC%E3%82%BF%E3%83%95%E3%83%AC%E3%83%BC%E3%83%A0%E3%82%92%E5%87%BA%E5%8A%9B%E3%81%99%E3%82%8B
+
 folder = "./jpg_convert"
 to_save = "./jpg_group1"  # 保存先のフォルダ（作成する）
 
@@ -25,11 +28,20 @@ labels = model.labels_  # 結果のラベル:整数値
 
 # 3. 結果
 print("Result...")
+cols = ['label', 'path']
+df = pd.DataFrame(columns=cols)
 for label, path in zip(labels, os.listdir(f'{folder}')):
-    os.makedirs(f'{to_save}/{label}', exist_ok=True)
-    shutil.copyfile(f'{folder}/{path}',
-                    f'{to_save}/{label}/{path}')
-    print(label, path)
+    # フォルダごとに画像を分類
+    # os.makedirs(f'{to_save}/{label}', exist_ok=True)
+    # shutil.copyfile(f'{folder}/{path}',
+    #                 f'{to_save}/{label}/{path}')
+    print([label, path])
+
+    # 分類結果のみを出力
+    record = pd.Series([label.copy(), path], index=df.columns)
+    df = df.append(record, ignore_index=True)  # 参照なので代入しないといけない。遅いらしい。
+df.to_csv("jpg_group_result1")
+
 
 print("END")
 
