74d9f40cce115f1222a367d4734ce581878514a6
KanoMrhr <sao.tkg.17@gmail.com>
2020-05-08 11:26:28 +0900

    3グループ分け→色で判断していそうで全然分類できていない。

---
+++ scikit-learn_k-means/test1.py
@@ -0,0 +1,35 @@
+# jpg_converterによって整理された画像をjpg_groupフォルダに分類する
+# https://webbibouroku.com/Blog/Article/sklearn-cluster-flag
+
+import os
+import numpy as np
+import shutil  # ファイル操作用
+from skimage import io  # 手もとの画像を使うとき？はdataではできなさそうだった。
+from sklearn.cluster import KMeans
+
+folder = "./jpg_convert"
+to_save = "./jpg_group1"  # 保存先のフォルダ（作成する）
+
+print("Start")
+# 1. 画像を読み込み、3次元→2次元配列データに変換
+jpgs = np.array([
+    io.imread(f'{folder}/{path}') for path in os.listdir(f'{folder}')
+])
+jpgs = jpgs.reshape(len(jpgs), -1).astype(np.float64)
+# 次元を減らす(100, 200, 3)->(100, 600)
+
+# 2. グループ分け学習
+print("learning...")
+model = KMeans(n_clusters=3).fit(jpgs)
+labels = model.labels_  # 結果のラベル:整数値
+
+# 3. 結果
+print("Result...")
+for label, path in zip(labels, os.listdir(f'{folder}')):
+    os.makedirs(f'{to_save}/{label}', exist_ok=True)
+    shutil.copyfile(f'{folder}/{path}',
+                    f'{to_save}/{label}/{path}')
+    print(label, path)
+
+print("END")
+
