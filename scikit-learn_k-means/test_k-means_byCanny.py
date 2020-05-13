7f7ae38df799e4f9e634e55ac7bd2b6e11aa120d
KanoMrhr <sao.tkg.17@gmail.com>
2020-05-13 11:41:26 +0900

    ファイルの内容を修正

--- scikit-learn_k-means/test_k-means_byCanny.py
+++ scikit-learn_k-means/test_k-means_byCanny.py
@@ -8,6 +8,7 @@ import numpy as np
 import shutil  # ファイル操作用
 from skimage import io  # 手もとの画像を使うとき？はdataではできなさそうだった。
 from sklearn.cluster import KMeans
+import re
 
 import pandas as pd  # ログをcsvでも保管しておく
 # https://pythondatascience.plavox.info/pandas/%E3%83%87%E3%83%BC%E3%82%BF%E3%83%95%E3%83%AC%E3%83%BC%E3%83%A0%E3%82%92%E5%87%BA%E5%8A%9B%E3%81%99%E3%82%8B
@@ -30,17 +31,17 @@ labels = model.labels_  # 結果のラベル:整数値
 
 # 3. 結果
 print("Result...")
-cols = ['label', 'path']
+cols = ['path', 'folder', 'label']
 df = pd.DataFrame(columns=cols)
 for label, path in zip(labels, os.listdir(f'{folder}')):
     # フォルダごとに画像を分類
-    os.makedirs(f'{to_save}/{label}', exist_ok=True)
-    shutil.copyfile(f'{folder}/{path}',
-                    f'{to_save}/{label}/{path}')
-    print([label, path])
+    # os.makedirs(f'{to_save}/{label}', exist_ok=True)
+    # shutil.copyfile(f'{folder}/{path}',
+    #                 f'{to_save}/{label}/{path}')
+    # print([label, path])
 
     # 分類結果のみを出力
-    record = pd.Series([label.copy(), path], index=df.columns)
+    record = pd.Series([path, re.sub("\d*.jpg", "", path), label.copy()], index=df.columns)
     df = df.append(record, ignore_index=True)  # 参照なので代入しないといけない。遅いらしい。
 df.to_csv("jpg_group_result1canny")
 
